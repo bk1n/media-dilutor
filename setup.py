fa6503492e1de5ab17d9e1a39a6cc8ccd8ad358a
Ben King <75484578+bk1n@users.noreply.github.com>
2024-04-11 23:39:06 +0100

    shift file structure

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='mediadilutor',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(where='src'),
     url='https://github.com/bk1n/media-dilutor',
     author='Ben King',
-    description='Package to calculate dilutions'
+    description='Package to calculate dilutions',
+    package_dir={'': 'src'}
 )
---
+++ test.py
@@ -0,0 +1,1 @@
+from mediadilutor import additive_dilution
